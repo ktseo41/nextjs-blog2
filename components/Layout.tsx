d4cef9f99164b6a56fe4111761473018cba3c28e
ktseo41 <ktseo41@gmail.com>
2021-02-09 20:45:25 +0900

    feat: Mui의 AppBar를 적용
    -  사용하지 않는 footer는 삭제
    -  Container를 이용해 horizontal center
    -  App Bar에서 사용하는 theme 적용

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Head from "next/head";
 import styles from "./layout.module.css";
+import TopAppBar from "./TopAppBar";
+import { Container } from "@material-ui/core";
 
 export default function Layout({ children }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link
-          rel="shortcut icon"
-          href="/noun_Whale Tail_3413920.ico"
-          type="image/x-icon"
-        />
-        <title>bohyeon blog</title>
-      </Head>
-      <header className={styles.header}>
-        <h2>bohyeon's blog</h2>
-      </header>
-      <section className={styles.body}>{children}</section>
-      <footer className={styles.footer}>
-        footer
-      </footer>
-    </div>
+    // <div className={styles.container}>
+      <Container disableGutters={true}>
+        <TopAppBar />
+        <Head>
+          <link
+            rel="shortcut icon"
+            href="/noun_Whale Tail_3413920.ico"
+            type="image/x-icon"
+          />
+          <title>bohyeon blog</title>
+        </Head>
+        <header className={styles.header}>
+          <h2>안녕하세요</h2>
+        </header>
+        <section className={styles.body}>{children}</section>
+      </Container>
   );
 }
---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "rgba(28, 71, 222, 0.87)",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#ffffff",
+    },
+  },
+});
+
+export default theme;
