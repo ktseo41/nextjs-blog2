bcee663d95614e41beda6363ba9877ec7cde5bee
ktseo41 <ktseo41@gmail.com>
2021-02-12 00:49:50 +0900

    feat: 첫 페이지에 환영 문구 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { getAllPostIds } from "../lib/posts";
 import Link from "next/link";
+import { Box, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => {
+  return {
+        welcomeBox: {
+          height: theme.spacing(19),
+          placeItems: "center",
+          textAlign: "center",
+        },
+      };
+;;
+});
 
 export default function Home({ ids }) {
+  const classes = useStyles();
+
   return (
     <>
+      <Box display="grid" className={classes.welcomeBox}>
+        <Typography variant="h5">
+          안녕하세요!
+          <Typography variant="body1">제 블로그에 오신걸 환영합니다!</Typography>
+        </Typography>
+      </Box>
       {ids.map(({ params: { id } }, idx) => {
         return (
           <div key={idx}>
