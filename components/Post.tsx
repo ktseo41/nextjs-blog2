53bdf38f968c731a9070a0207ec20e8a4bf7958f
ktseo41 <ktseo41@gmail.com>
2021-02-12 23:53:05 +0900

    feat: Post 컴포넌트에 props 추가

--- components/Post.tsx
+++ components/Post.tsx
@@ -11,22 +11,36 @@ import {
 } from "@material-ui/core";
 import Image from "next/image";
 
-export function ImageTopPost() {
+export function ImageTopPost({
+  title = "Card Title",
+  createdTime = 2,
+  modifiedTime,
+  description = "card description. Ipsum nisi sunt dolor adipisicing velit duis velit ut ullamco",
+  chips = ["css", "javascript", "web"],
+  imageSrc = "https://picsum.photos/344/194",
+}: {
+  title: string;
+  createdTime: number;
+  modifiedTime: number;
+  description: string;
+  chips: string[];
+  imageSrc: string;
+}) {
   return (
     <Card elevation={0}>
       <CardActionArea>
-        <Image alt="random" width={344} height={194} src={"https://picsum.photos/344/194"} />
+        <Image alt="random" width={344} height={194} src={imageSrc} />
         {/* 기본 값이 16이지만.. */}
         <CardContent style={{ padding: 16 }}>
-          <Typography variant="h6">Card Title</Typography>
-          <Typography variant="caption">2 days ago</Typography>
+          <Typography variant="h6">{title}</Typography>
+          <Typography variant="caption">{createdTime} days ago</Typography>
           <Typography style={{ marginTop: 8 }} variant="body2">
-            card description. Ipsum nisi sunt dolor adipisicing velit duis velit ut ullamco
+            {description}
           </Typography>
           <CardActions style={{ padding: "8px 0 0 0" }}>
-            <Chip clickable variant="outlined" size="small" label="css" />
-            <Chip clickable variant="outlined" size="small" label="javascript" />
-            <Chip clickable variant="outlined" size="small" label="web" />
+            {chips.map((chip) => {
+              return <Chip clickable variant="outlined" size="small" label={chip} />;
+            })}
           </CardActions>
         </CardContent>
       </CardActionArea>
@@ -34,7 +48,21 @@ export function ImageTopPost() {
   );
 }
 
-export function ImageRightPost() {
+export function ImageRightPost({
+  title = "Card Title",
+  createdTime = 12,
+  modifiedTime,
+  description = "card description. Ipsum nisi sunt dolor adipisicing",
+  chips = ["css", "javascript", "web"],
+  imageSrc = "/bohyeon.jpeg",
+}: {
+  title: string;
+  createdTime: number;
+  modifiedTime: number;
+  description: string;
+  chips: string[];
+  imageSrc: string;
+}) {
   return (
     <Card elevation={0}>
       <CardActionArea>
@@ -42,20 +70,18 @@ export function ImageRightPost() {
           <Box style={{ display: "grid", gridTemplateColumns: "repeat(2, auto)", columnGap: 16 }}>
             <Box>
               <Typography gutterBottom={false} variant="h6">
-                Card Title
+                {title}
               </Typography>
-              <Typography variant="body2">
-                card description. Ipsum nisi sunt dolor adipisicing
-              </Typography>
+              <Typography variant="body2">{description}</Typography>
             </Box>
-            <Image alt="random2" width={72} height={72} src={"https://picsum.photos/72/72"} />
+            <Image alt="random2" width={72} height={72} src={imageSrc} />
           </Box>
           <Box display="flex" alignItems="center" style={{ paddingTop: 16 }}>
-            <Typography variant="caption">12 days ago</Typography>
+            <Typography variant="caption">{createdTime} days ago</Typography>
             <CardActions style={{ padding: 0, marginLeft: 16 }}>
-              <Chip clickable variant="outlined" size="small" label="css" />
-              <Chip clickable variant="outlined" size="small" label="javascript" />
-              <Chip clickable variant="outlined" size="small" label="web" />
+              {chips.map((chip) => {
+                return <Chip clickable variant="outlined" size="small" label={chip} />;
+              })}
             </CardActions>
           </Box>
         </CardContent>
