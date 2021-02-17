383e87a41f42fa589760e9f18cedc7b738897aee
ktseo41 <ktseo41@gmail.com>
2021-02-18 04:59:15 +0900

    feat: 최근 생성, 수정된 문서들의  header를 가져오는 함수 작성

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import readline from "readline";
 import * as dotenv from "dotenv";
 import marked from "marked";
 import path from "path";
@@ -23,6 +24,72 @@ export function getAllPostIds() {
         return [...accu];
       return [...accu, curr];
     }, []);
+}
+
+export type WikiHeader = {
+  title: string;
+  created: string;
+  modified: string;
+  tag: string;
+};
+
+async function getAllHeaders() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const onlyPostsFileNames = fileNames.filter((fileName) => {
+    if (fileName.includes(".un~") || fileName.includes("~")) return false;
+    if (fileName === "wiki" || fileName === "diary" || fileName === "index.md") return false;
+    return true;
+  });
+
+  return Promise.all(
+    onlyPostsFileNames.map<Promise<WikiHeader>>(async (fileName) => {
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileStream = fs.createReadStream(fullPath);
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+      });
+
+      const header = {};
+      let headerStarted = false;
+
+      for await (const line of rl) {
+        if (line === "---" && headerStarted) break;
+        if (line === "---") {
+          headerStarted = true;
+          continue;
+        }
+
+        const seperateIndex = line.indexOf(":");
+        const [property, value] = [line.slice(0, seperateIndex), line.slice(seperateIndex + 1)];
+        header[property.trim()] = value.trim();
+      }
+
+      return header as WikiHeader;
+    })
+  );
+}
+
+export async function getRecentCreatedPosts(goalCount: number) {
+  const allHeaders = await getAllHeaders();
+
+  return allHeaders
+    .sort(
+      ({ created: createdA }, { created: createdB }) =>
+        new Date(createdB).getTime() - new Date(createdA).getTime()
+    )
+    .slice(0, goalCount);
+}
+
+export async function getRecentModifiedPosts(goalCount: number) {
+  const allHeaders = await getAllHeaders();
+
+  return allHeaders
+    .sort(
+      ({ modified: modifiedA }, { modified: modifiedB }) =>
+        new Date(modifiedB).getTime() - new Date(modifiedA).getTime()
+    )
+    .slice(0, goalCount);
 }
 
 export function getPostDatasFromId(id: string) {
