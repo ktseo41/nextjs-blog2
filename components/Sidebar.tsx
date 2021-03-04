210813b4f41d5adf26f5d241678c92badf5bb9b2
ktseo41 <ktseo41@gmail.com>
2021-03-05 02:35:40 +0900

    feat: 사이드바 카테고리 및 아이콘 지정

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,13 +8,30 @@ import {
   makeStyles,
   Typography,
 } from "@material-ui/core";
-import { Close, HeadsetSharp } from "@material-ui/icons";
+import {
+  AccountCircle,
+  BuildOutlined,
+  ChatOutlined,
+  ChromeReaderModeOutlined,
+  Close,
+  CreateOutlined,
+  DescriptionOutlined,
+  KeyboardOutlined,
+  ListAltOutlined,
+  PlusOneOutlined,
+} from "@material-ui/icons";
 import React, { Dispatch, SetStateAction } from "react";
+import { CategoryLabel } from "../constants";
 import CategoryListItem from "./CategoryListItem";
 
 type SidebarProps = {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+type ChipProps = {
+  label: string;
+  count: number;
 };
 
 const useStyles = makeStyles((theme) => {
@@ -24,14 +41,31 @@ const useStyles = makeStyles((theme) => {
       width: theme.breakpoints.width("sm") * 0.5,
       maxWidth: theme.breakpoints.width("sm") * 0.5,
     },
+    // https://stackoverflow.com/questions/53772429/material-ui-how-can-i-style-the-scrollbar-with-css-in-js
     chipBox: {
       padding: "0px 0px 16px 16px",
       display: "flex",
       flexFlow: "nowrap",
       overflowY: "scroll",
     },
+    chipCount: {
+      marginLeft: 4,
+      color: "rgba(0, 0, 0, 0.6)",
+    },
   };
 });
+
+function ChipLabel(props: ChipProps) {
+  const { label, count } = props;
+  const classes = useStyles();
+
+  return (
+    <>
+      {label}
+      <span className={classes.chipCount}>{count}</span>
+    </>
+  );
+}
 
 export default function Sidebar(props: SidebarProps) {
   const { open, setOpen } = props;
@@ -47,7 +81,7 @@ export default function Sidebar(props: SidebarProps) {
       <Box className={classes.drawerContainer}>
         <Box style={{ display: "flex", flexDirection: "column" }}>
           <IconButton
-            style={{ padding: "16px 0px", alignSelf: "flex-end" }}
+            style={{ padding: 0, margin: "16px 0px", alignSelf: "flex-end" }}
             onClick={() => setOpen(false)}
           >
             <Close />
@@ -56,17 +90,17 @@ export default function Sidebar(props: SidebarProps) {
         </Box>
         <Box>
           <Typography style={{ paddingTop: 16 }} variant="h6">
-            Headline 6
+            {"보현's blog"}
           </Typography>
           <List>
-            <CategoryListItem icon={<HeadsetSharp />} label="test" />
-            <CategoryListItem icon={<HeadsetSharp />} label="test" />
-            <CategoryListItem icon={<HeadsetSharp />} label="test" />
+            <CategoryListItem icon={<AccountCircle />} label={CategoryLabel.ABOUT_ME} />
+            <CategoryListItem icon={<BuildOutlined />} label={CategoryLabel.ABOUT_BLOG} />
+            <CategoryListItem icon={<DescriptionOutlined />} label={CategoryLabel.RESUME} />
           </List>
           <Divider />
           <Box>
             <Typography style={{ padding: "8px 0px" }} variant="subtitle1">
-              Subtitle 1
+              {CategoryLabel.BY_TAGS}
             </Typography>
             <Box className={classes.chipBox}>
               {[
@@ -78,9 +112,10 @@ export default function Sidebar(props: SidebarProps) {
               ].map((eachChip: { label: string; count: number }) => {
                 return (
                   <Chip
+                    variant="outlined"
                     style={{ marginRight: 8 }}
                     key={eachChip.label}
-                    label={`${eachChip.label} ${eachChip.count}`}
+                    label={<ChipLabel label={eachChip.label} count={eachChip.count} />}
                   />
                 );
               })}
@@ -89,9 +124,12 @@ export default function Sidebar(props: SidebarProps) {
           </Box>
           <Box>
             <List>
-              <CategoryListItem icon={<HeadsetSharp />} label="test" />
-              <CategoryListItem icon={<HeadsetSharp />} label="test" />
-              <CategoryListItem icon={<HeadsetSharp />} label="test" />
+              <CategoryListItem icon={<ListAltOutlined />} label={CategoryLabel.VIEW_ALL} />
+              <CategoryListItem icon={<PlusOneOutlined />} label={CategoryLabel.RECENTLY_CREATED} />
+              <CategoryListItem icon={<CreateOutlined />} label={CategoryLabel.RECENTLY_MODIFIED} />
+              <CategoryListItem icon={<KeyboardOutlined />} label={CategoryLabel.DEVELOPMENT} />
+              <CategoryListItem icon={<ChromeReaderModeOutlined />} label={CategoryLabel.READING} />
+              <CategoryListItem icon={<ChatOutlined />} label={CategoryLabel.PERSONAL_LOG} />
             </List>
           </Box>
         </Box>
--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,16 @@ export enum WikiHeaderKey {
   TAG = "tag",
   PUBLISH = "publish",
 }
+
+export enum CategoryLabel {
+  ABOUT_ME = "저에 대해",
+  ABOUT_BLOG = "블로그는 이렇게 만들었어요",
+  RESUME = "이력서",
+  BY_TAGS = "태그별 보기",
+  VIEW_ALL = "전체 보기",
+  RECENTLY_CREATED = "최근 작성한 글",
+  RECENTLY_MODIFIED = "최근 수정한 글",
+  DEVELOPMENT = "개발",
+  READING = "독서",
+  PERSONAL_LOG = "잡담",
+}
